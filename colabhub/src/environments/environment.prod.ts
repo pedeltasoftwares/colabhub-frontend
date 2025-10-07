9f00d279c9b9d2df3b0132b2b564ed13225ba128
Pedelta Software <144169025+pedeltasoftwares@users.noreply.github.com>
2025-10-07 15:43:00 -0500

    update

--- colabhub/src/environments/environment.prod.ts
+++ colabhub/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 // environment.prod.ts (PROD)
 export const environment = {
   production: true,
-  apiUrl: 'https://backend-colabhub-prod-fafsdzepaghxakhe.canadacentral-01.azurewebsites.net'
+  apiUrl: 'https://backend-colabhub-prod-fafsdzepaghxakhe.eastus2-01.azurewebsites.net'
 };
